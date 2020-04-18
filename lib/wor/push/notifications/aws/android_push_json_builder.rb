b8afb89dded5fdfc1604bef58434e97543310724
LVSMix <lvsmix@gmail.com>
2020-04-18 17:37:58 -0300

    modificacion android

--- lib/wor/push/notifications/aws/android_push_json_builder.rb
+++ lib/wor/push/notifications/aws/android_push_json_builder.rb
@@ -5,7 +5,8 @@ module Wor
         class AndroidPushJsonBuilder
           class << self
             def build_json(message_content)
-              { GCM: { data: message_content }.to_json }
+              #{ GCM: { data: message_content }.to_json }
+              { GCM: { notification: message_content }.to_json }
             end
           end
         end
